24050679687948e85f95bc061d9785f69f739805
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-08-11 11:47:50 +0300

    Version 2

--- js/task-08.js
+++ js/task-08.js
@@ -3,7 +3,7 @@ const renderBtn = document.querySelector('button');
 const list = document.querySelector('#boxes');
 let value = [];
 let salary = 0;
-let devSum = 0;
+
 
 input.addEventListener('blur', onGetValue);
 renderBtn.addEventListener('click', onRenderSalary);
@@ -17,23 +17,25 @@ function onRenderSalary() {
   let sum = 0;
 
   const element = document.createElement('li');
+  element.classList.add('boxes__element')
   const p = document.createElement('p');
+  element.classList.add('boxes__text')
 
   value[0].forEach(element => {
-    devSum += 1;
+    
     sum = sum + +element;
   });
   if (sum > 41) {
     salary += sum * 0.6;
     console.log(salary);
-    p.textContent = `Зарплата:${salary}грн, за доставку ${devSum}грн`;
+    p.textContent = `Зарплата: ${Math.round(salary)}грн.`;
     element.appendChild(p);
     console.log(element);
     list.append(element);
     value = [];
   } else {
     salary += 25;
-    p.textContent = `Зарплата:${salary}грн. За доставку ${devSum}грн`;
+    p.textContent = `Зарплата: ${Math.round(salary)}грн.`;
     element.appendChild(p);
     list.append(element);
     value = [];
